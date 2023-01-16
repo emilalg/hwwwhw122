a3d8891ab546ec5ae2023b9680460b05d9827109
emilalg <Emil.Algars@metropolia.fi>
2023-01-16 19:36:58 +0200

    test

---
+++ FactoryMethod/Rehtori.java
@@ -0,0 +1,10 @@
+package factorymethod;
+
+public class Rehtori extends AterioivaOtus{
+
+
+    public Juoma createJuoma() {
+        return new tee();
+    }
+
+}
---
+++ FactoryMethod/tee.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class tee implements Juoma{
+
+    public String toString(){
+        return "tee";
+    }
+
+}
